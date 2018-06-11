ab15b19b6104c1d81a909c925fa1bdfca88882d8
Justine Bricknell <justinegayton@gmail.com>
2018-06-11 18:49:09 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
     counter = 0
-    board.each do |token|
-    if token == "X" || token == "O"
+    board.each do |turn|
+    if turn == "X" || turn == "O"
     counter += 1
   end
 end
@@ -11,9 +11,5 @@ end
 #return value from this is an integer
 
 def current_player(board)
-  if turn_count(board) % 2 == 0   #4 % 2 == 0 (4 is divided by 2 with a remainder of 0. yes)
-    return "X"
-  else
-    return "O"
-end
+turn_count(board).even? ? "X" : "O"
 end
